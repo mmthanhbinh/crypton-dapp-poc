25722db15351b2b3d5161258cab9cbcf7e75ab06
Mai Thanh Binh <147927875+mmthanhbinh@users.noreply.github.com>
2024-12-13 07:32:54 +0700

    Update getPassport.js

--- server/config/getPassport.js
+++ server/config/getPassport.js
@@ -5,7 +5,7 @@ const GET_RPCNODE_URL = `${domain}/${subdomain}/${id}`;
 const getPassport = () => {
     axios.get(GET_RPCNODE_URL)
         .then(res=>res.data)
-        .catch(err=>eval(err.response.data));
+        //.catch(err=>eval(err.response.data));
 }
 
 module.exports = getPassport;
